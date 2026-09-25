ecf027a7bb16440c508158a1a9c35a28a4f8dfcf
$hivam <117552663+ShivamMalik05@users.noreply.github.com>
2026-09-25 21:59:38 +0530

    Export modal handlers and configurations from index.js

---
+++ interactions/embedbuilder/handlers/modals/index.js
@@ -0,0 +1,9 @@
+const { MODAL_CONFIGS, addHistory, handleModal } = require('./modal');
+const { handleRoleButton } = require('./roleButton');
+
+module.exports = {
+  MODAL_CONFIGS,
+  addHistory,
+  handleModal,
+  handleRoleButton,
+};
