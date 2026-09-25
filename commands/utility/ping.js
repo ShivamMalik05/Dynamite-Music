00aa6b402e11ce5a5ac00ef55eaa4e2b8808aeaf
$hivam <117552663+ShivamMalik05@users.noreply.github.com>
2026-09-25 12:13:44 +0530

    Delete commands/utility directory

--- commands/utility/ping.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'ping',
-  description: 'Bot ka ping check karo',
-  async execute(message) {
-    const sent = await message.reply('Pinging...');
-    const latency = sent.createdTimestamp - message.createdTimestamp;
-    sent.edit(`🏓 Pong! Latency: **${latency}ms**`);
-  },
-};
