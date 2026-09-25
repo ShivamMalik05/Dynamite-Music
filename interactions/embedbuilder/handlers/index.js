a1a9cf20e8e6887aab9acca82fb00d9273b3c33f
$hivam <117552663+ShivamMalik05@users.noreply.github.com>
2026-09-25 21:14:03 +0530

    Refactor imports for button and modal handlers

--- interactions/embedbuilder/handlers/index.js
+++ interactions/embedbuilder/handlers/index.js
@@ -1,11 +1,15 @@
-const { handleButton } = require('./buttons');
-const { handleModal, handleRoleButton } = require('./modals');
+const { handleButton } = require('./buttons/button');
+const { handleSelect } = require('./buttons/select');
+const { handleModal } = require('./modals/modal');
+const { handleRoleButton } = require('./modals/roleButton');
 
 module.exports = {
   handleButton,
+  handleSelect,
   handleModal,
   handleRoleButton,
   isEmbedButton: (id) => id.startsWith('embed_') || id.startsWith('eb_'),
+  isEmbedSelect: (id) => id.startsWith('eb_') && id.endsWith('_select'),
   isEmbedModal: (id) => id.startsWith('modal_'),
   isRoleButton: (id) => id.startsWith('eb_action_role_'),
 };
