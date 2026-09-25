95f086365e3fe326651c97209f814b159e661f78
$hivam <117552663+ShivamMalik05@users.noreply.github.com>
2026-09-25 14:57:26 +0530

    Refactor userinfo command to use SlashCommandBuilder

--- commands/utility/userinfo.js
+++ commands/utility/userinfo.js
@@ -1,15 +1,31 @@
-const emojis = require('../../emojis/emojis');
+const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
   name: 'userinfo',
   description: 'Show user information',
-  async execute(message) {
-    const user = message.mentions.users.first() || message.author;
-    const member = message.guild.members.cache.get(user.id);
+  data: new SlashCommandBuilder()
+    .setName('userinfo')
+    .setDescription('Show user information')
+    .addUserOption(option =>
+      option.setName('user')
+        .setDescription('The user to get info about')
+        .setRequired(false)
+    ),
+
+  async execute(context) {
+    let user, member;
+
+    if (context.isChatInputCommand && context.isChatInputCommand()) {
+      user = context.options.getUser('user') || context.user;
+      member = await context.guild.members.fetch(user.id);
+    } else {
+      user = context.mentions.users.first() || context.author;
+      member = context.guild.members.cache.get(user.id);
+    }
 
     const embed = {
-      color: 0x0099ff,
-      title: `${emojis.user} User: ${user.tag}`,
+      color: 0x9B59B6,
+      title: `User: ${user.tag}`,
       thumbnail: { url: user.displayAvatarURL({ dynamic: true }) },
       fields: [
         { name: 'ID', value: user.id, inline: true },
@@ -19,6 +35,10 @@ module.exports = {
       ],
     };
 
-    message.reply({ embeds: [embed] });
+    if (context.isChatInputCommand && context.isChatInputCommand()) {
+      await context.reply({ embeds: [embed] });
+    } else {
+      context.reply({ embeds: [embed] });
+    }
   },
 };
