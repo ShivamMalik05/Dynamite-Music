1744c49b63081f0a089317761d8143fd7d9c35b6
$hivam <117552663+ShivamMalik05@users.noreply.github.com>
2026-09-25 15:03:37 +0530

    Refactor nickname command to use SlashCommandBuilder

--- commands/moderation/nickname.js
+++ commands/moderation/nickname.js
@@ -1,25 +1,41 @@
-const emojis = require('../../emojis/emojis');
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 
 module.exports = {
   name: 'nickname',
   description: 'Change a user nickname',
-  async execute(message, args) {
-    if (!message.member.permissions.has('ManageNicknames')) {
-      return message.reply(`${emojis.error} You do not have permission!`);
+  data: new SlashCommandBuilder()
+    .setName('nickname')
+    .setDescription('Change a user nickname')
+    .addUserOption(option =>
+      option.setName('user').setDescription('User').setRequired(true))
+    .addStringOption(option =>
+      option.setName('nickname').setDescription('New nickname').setRequired(true))
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageNicknames),
+
+  async execute(context, args) {
+    let member, nickname;
+
+    if (context.isChatInputCommand && context.isChatInputCommand()) {
+      const user = context.options.getUser('user');
+      member = await context.guild.members.fetch(user.id);
+      nickname = context.options.getString('nickname');
+    } else {
+      if (!context.member.permissions.has('ManageNicknames')) {
+        return context.reply('You do not have permission!');
+      }
+      member = context.mentions.members.first();
+      if (!member) return context.reply('Mention a user!');
+      nickname = args.slice(1).join(' ');
+      if (!nickname) return context.reply('Provide a nickname!');
     }
 
-    const member = message.mentions.members.first();
-    if (!member) return message.reply(`${emojis.error} Mention a user!`);
+    await member.setNickname(nickname);
+    const msg = `Changed **${member.user.tag}** nickname to **${nickname}**.`;
 
-    const nickname = args.slice(1).join(' ');
-    if (!nickname) return message.reply(`${emojis.error} Provide a nickname!`);
-
-    try {
-      await member.setNickname(nickname);
-      message.reply(`${emojis.success} Changed **${member.user.tag}** nickname to **${nickname}**.`);
-    } catch (error) {
-      console.error(error);
-      message.reply(`${emojis.error} Failed to change nickname.`);
+    if (context.isChatInputCommand && context.isChatInputCommand()) {
+      await context.reply(msg);
+    } else {
+      context.reply(msg);
     }
   },
 };
