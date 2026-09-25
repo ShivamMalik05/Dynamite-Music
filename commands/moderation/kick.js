f43e875157685629bbb6bc18e72aa17ff1f38bfd
$hivam <117552663+ShivamMalik05@users.noreply.github.com>
2026-09-25 14:59:06 +0530

    Refactor kick command to use slash commands

--- commands/moderation/kick.js
+++ commands/moderation/kick.js
@@ -1,30 +1,38 @@
-const emojis = require('../../emojis/emojis');
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 
 module.exports = {
   name: 'kick',
   description: 'Kick a user',
-  async execute(message, args) {
-    if (!message.member.permissions.has('KickMembers')) {
-      return message.reply(`${emojis.error} You do not have permission!`);
-    }
+  data: new SlashCommandBuilder()
+    .setName('kick')
+    .setDescription('Kick a user')
+    .addUserOption(option =>
+      option.setName('user').setDescription('User to kick').setRequired(true))
+    .addStringOption(option =>
+      option.setName('reason').setDescription('Reason').setRequired(false))
+    .setDefaultMemberPermissions(PermissionFlagsBits.KickMembers),
 
-    const member = message.mentions.members.first();
-    if (!member) {
-      return message.reply(`${emojis.error} Mention a user to kick!`);
-    }
+  async execute(context, args) {
+    if (context.isChatInputCommand && context.isChatInputCommand()) {
+      const user = context.options.getUser('user');
+      const reason = context.options.getString('reason') || 'No reason provided';
+      const member = await context.guild.members.fetch(user.id);
 
-    if (!member.kickable) {
-      return message.reply(`${emojis.error} Cannot kick this user!`);
-    }
-
-    const reason = args.slice(1).join(' ') || 'No reason provided';
-
-    try {
+      if (!member.kickable) {
+        return context.reply({ content: 'Cannot kick this user!', ephemeral: true });
+      }
       await member.kick(reason);
-      message.reply(`${emojis.success} Kicked **${member.user.tag}**. Reason: ${reason}`);
-    } catch (error) {
-      console.error(error);
-      message.reply(`${emojis.error} Failed to kick this user.`);
+      await context.reply(`Kicked **${user.tag}**. Reason: ${reason}`);
+    } else {
+      if (!context.member.permissions.has('KickMembers')) {
+        return context.reply('You do not have permission!');
+      }
+      const member = context.mentions.members.first();
+      if (!member) return context.reply('Mention a user to kick!');
+      if (!member.kickable) return context.reply('Cannot kick this user!');
+      const reason = args.slice(1).join(' ') || 'No reason provided';
+      await member.kick(reason);
+      context.reply(`Kicked **${member.user.tag}**. Reason: ${reason}`);
     }
   },
 };
