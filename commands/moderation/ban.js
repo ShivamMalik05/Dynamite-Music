642895660a69dc953dfe8dfcb4ebde5278f510fc
$hivam <117552663+ShivamMalik05@users.noreply.github.com>
2026-09-25 14:58:42 +0530

    Refactor ban command to use SlashCommandBuilder

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -1,30 +1,38 @@
-const emojis = require('../../emojis/emojis');
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 
 module.exports = {
   name: 'ban',
   description: 'Ban a user',
-  async execute(message, args) {
-    if (!message.member.permissions.has('BanMembers')) {
-      return message.reply(`${emojis.error} You do not have permission!`);
-    }
+  data: new SlashCommandBuilder()
+    .setName('ban')
+    .setDescription('Ban a user')
+    .addUserOption(option =>
+      option.setName('user').setDescription('User to ban').setRequired(true))
+    .addStringOption(option =>
+      option.setName('reason').setDescription('Reason').setRequired(false))
+    .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers),
 
-    const member = message.mentions.members.first();
-    if (!member) {
-      return message.reply(`${emojis.error} Mention a user to ban!`);
-    }
+  async execute(context, args) {
+    if (context.isChatInputCommand && context.isChatInputCommand()) {
+      const user = context.options.getUser('user');
+      const reason = context.options.getString('reason') || 'No reason provided';
+      const member = await context.guild.members.fetch(user.id);
 
-    if (!member.bannable) {
-      return message.reply(`${emojis.error} Cannot ban this user!`);
-    }
-
-    const reason = args.slice(1).join(' ') || 'No reason provided';
-
-    try {
+      if (!member.bannable) {
+        return context.reply({ content: 'Cannot ban this user!', ephemeral: true });
+      }
       await member.ban({ reason });
-      message.reply(`${emojis.success} Banned **${member.user.tag}**. Reason: ${reason}`);
-    } catch (error) {
-      console.error(error);
-      message.reply(`${emojis.error} Failed to ban this user.`);
+      await context.reply(`Banned **${user.tag}**. Reason: ${reason}`);
+    } else {
+      if (!context.member.permissions.has('BanMembers')) {
+        return context.reply('You do not have permission!');
+      }
+      const member = context.mentions.members.first();
+      if (!member) return context.reply('Mention a user to ban!');
+      if (!member.bannable) return context.reply('Cannot ban this user!');
+      const reason = args.slice(1).join(' ') || 'No reason provided';
+      await member.ban({ reason });
+      context.reply(`Banned **${member.user.tag}**. Reason: ${reason}`);
     }
   },
 };
