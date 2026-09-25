82bec360c4d1d496d5c4f76857233a40d3563fff
$hivam <117552663+ShivamMalik05@users.noreply.github.com>
2026-09-25 22:19:07 +0530

    Add logging configuration in logs.js

---
+++ config/logs.js
@@ -0,0 +1,35 @@
+module.exports = {
+  channels: {
+    moderation: '',
+    messages: '',
+    members: '',
+    channels: '',
+    roles: '',
+    voice: '',
+    server: '',
+  },
+
+  enabled: {
+    moderation: true,
+    messages: true,
+    members: true,
+    channels: true,
+    roles: true,
+    voice: true,
+    server: true,
+  },
+
+  colors: {
+    moderation: 0xED4245,
+    messages: 0xFEE75C,
+    members: 0x57F287,
+    channels: 0x5865F2,
+    roles: 0xEB459E,
+    voice: 0x1ABC9C,
+    server: 0x9B59B6,
+  },
+
+  ignoredChannels: [],
+  ignoredRoles: [],
+  ignoredUsers: [],
+};
