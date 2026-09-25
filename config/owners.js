d5f640d0b11fd8a1f9622f9a10536581e681191b
$hivam <117552663+ShivamMalik05@users.noreply.github.com>
2026-09-26 00:06:52 +0530

    Add bot owners configuration file

---
+++ config/owners.js
@@ -0,0 +1,10 @@
+// ============================================
+// BOT OWNERS CONFIG
+// Owners have FULL access to everything
+// ============================================
+
+module.exports = {
+  owners: [
+    '915174312707772427',
+  ],
+};
