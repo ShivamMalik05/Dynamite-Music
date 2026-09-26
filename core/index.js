87d30dd67f74c86d1dfe3579dd596619762e767e
$hivam <117552663+ShivamMalik05@users.noreply.github.com>
2026-09-26 20:44:51 +0530

    Create core/index.js to export module components

---
+++ core/index.js
@@ -0,0 +1,12 @@
+module.exports = {
+  config: require('./config'),
+  embeds: require('./embeds'),
+  emojis: require('./emojis'),
+  logger: require('./logger'),
+  permissions: require('./permissions'),
+  prefix: require('./prefix'),
+  nop: require('./nop'),
+  warnings: require('./warnings'),
+  autoAction: require('./autoAction'),
+  music: require('./music'),
+};
