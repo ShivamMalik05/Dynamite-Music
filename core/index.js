147d3dcb5694a0ce27e0e8a3fe0598c1458f3a57
$hivam <117552663+ShivamMalik05@users.noreply.github.com>
2026-09-26 18:27:49 +0530

    Create core/index.js to export modules

---
+++ core/index.js
@@ -0,0 +1,13 @@
+const warnings = require('./warnings');
+const config = require('./config');
+const embeds = require('./embeds');
+const autoAction = require('./autoAction');
+const permissions = require('./permissions');
+
+module.exports = {
+  warnings,
+  config,
+  embeds,
+  autoAction,
+  permissions,
+};
