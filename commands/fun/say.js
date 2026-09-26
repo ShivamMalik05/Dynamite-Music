3e07dc6a570113dc6b79067deff16777a47cb0ba
$hivam <117552663+ShivamMalik05@users.noreply.github.com>
2026-09-26 15:30:58 +0530

    Refactor 'say' command to use new builders

--- commands/fun/say.js
+++ commands/fun/say.js
@@ -1,25 +1,60 @@
-const { SlashCommandBuilder } = require('discord.js');
+const {
+  SlashCommandBuilder,
+  ContainerBuilder,
+  TextDisplayBuilder,
+  SeparatorBuilder,
+} = require('discord.js');
+const emojis = require('../../emojis/emojis');
+
+function makeSep() {
+  try {
+    const sep = new SeparatorBuilder();
+    if (typeof sep.setSpacing === 'function') sep.setSpacing(1);
+    if (typeof sep.setDivider === 'function') sep.setDivider(true);
+    return sep;
+  } catch {
+    return { type: 14, divider: true, spacing: 1 };
+  }
+}
 
 module.exports = {
   name: 'say',
   description: 'Make the bot say something',
+  category: 'Fun',
   data: new SlashCommandBuilder()
     .setName('say')
     .setDescription('Make the bot say something')
     .addStringOption(option =>
-      option.setName('text').setDescription('Text to say').setRequired(true)),
+      option.setName('text')
+        .setDescription('Text to say')
+        .setRequired(true))
+    .addChannelOption(option =>
+      option.setName('channel')
+        .setDescription('Channel to send to (defaults to current)')
+        .setRequired(false)),
 
   async execute(context, args) {
-    let text;
+    const isSlash = context.isChatInputCommand && context.isChatInputCommand();
+    let text, channel;
 
-    if (context.isChatInputCommand && context.isChatInputCommand()) {
+    if (isSlash) {
       text = context.options.getString('text');
-      await context.reply(text);
+      channel = context.options.getChannel('channel') || context.channel;
     } else {
+      // Delete user's message
+      setTimeout(() => context.delete().catch(() => {}), 500);
+
       text = args.join(' ');
-      if (!text) return context.reply('Provide some text!');
-      await context.delete().catch(() => {});
-      context.channel.send(text);
+      channel = context.channel;
+
+      if (!text) {
+        const msg = await context.reply(`${emojis.error} Provide some text!`);
+        setTimeout(() => msg.delete().catch(() => {}), 3000);
+        return;
+      }
     }
+
+    // Send message (no embed, just plain text)
+    await channel.send(text);
   },
 };
